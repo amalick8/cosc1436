521f9c0c7afe02ae2823db9f008adbc1e99a4a22
ammar_malick_8 <ammarmalick2006@gmail.com>
2025-08-28 21:34:26 -0500

    end of august 28th class

---
+++ classwork/PartsOfLanguage/PartsOfLanguage/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//Keywords are in blue - have special meaning to language
+
+/*This is 
+  a multiline
+  comment
+ */
+
+//This is also
+//a multiline
+//comment
+
+void main()
+{   
+    std::cout << "Hello World";
+}
+
