9a12e2d341ad94008a6805348c1aca74100eca65
ammar_malick_8 <ammarmalick2006@gmail.com>
2025-09-02 21:31:23 -0500

    Variables and literals

---
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -0,0 +1,45 @@
+// Chapter2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// For a lab:
+// Your name
+// Lab 1
+// COSC 1436 Fall 2025
+// 
+// 
+// #include is a Preprocessor directive, it takes the contents of the file
+// which is <iostream> in this case, <iostream> is very large, the compiler sees
+// <iostream> and uses that code instead of wasting your space, kind of like an 
+// attachment
+#include <iostream>
+
+int main()
+{
+  //std::cout << "Hello World" << " Your Name" << std::endl;
+  //std::cout << "You are " << 20 << " years old" << std::endl;
+
+    //Identical to << endl
+ //std::cout << "Hello World" << " Your Name\n";
+
+  //std::cout << "Hello World\nYour Name";
+    //String literals "..." A literal is a read only piece of data
+    // "Whatever" Just a block of text
+    // Escape sequence - special meaning to the compiler
+    //      - Starts with \ followed by a character
+    //      - \t does 3-4 spots of spaces 
+    //      - \n does new line
+    //      - \" for double quote
+    //      - \\ for slash
+
+    //Integer literals - one or more digits, used when you need constant numerals
+
+    //Floating point literals - one or more digits + fraction like 
+    // 3.14159 which is pi or 3E10, -3E10 3E-10
+
+    // Variable - Named memory location where data is stored
+
+    std::string name; 
+    name = "Ammar";
+    std::cout << "Hello World " << name;
+
+}
+
