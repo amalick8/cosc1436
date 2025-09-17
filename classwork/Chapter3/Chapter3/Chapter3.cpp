2953665b7b1eeaf468c787290e22d0d17853b809
amalick8 <ammarmalick2006@gmail.com>
2025-09-16 20:38:06 -0500

    update

--- classwork/Chapter3/Chapter3/Chapter3.cpp
+++ classwork/Chapter3/Chapter3/Chapter3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 void main()
 {
@@ -52,6 +53,58 @@ void main()
     std::cout << smallValue << " " << largeValue << std::endl;
 
     //Type coercion - compiler implicitly converts an expression's type to a larger type
+
+    //Type casting - you explicitly convert a type to another type
+
+    int left = 10;
+    int right = 3;
+
+    double intDivision = 10 / 3; //int
+    intDivision = 10.0 / 3; // double / int => double
+    intDivision = 10 / 3.0; // int / double => double
+    intDivision = left / right; //int / int => int
+
+    intDivision = (double)(left / right); //int / int => int => (double)int
+
+    intDivision = static_cast<double>(left) / right; // (double)int / int => double
+    intDivision = left / (double)right; // int / (double)int => double
+
+    //This is wrong and will not produce 5
+    intDivision = (int)"5";
+    //intDivision = static_cast<int>("5") // Compiler error
+
+    //Math functions
+    //pow(x, y) => power X to the Y power => x^y
+    //sqrt(x) => square root of X
+    //
+    //ceil(x)=> Smallest possible value >=X
+    //floor(x)=> Largest possible integral value <=X
+
+    // round(x)=> Rounds a float to an int using midpoint rounding
+    // trunc(x)=> Truncates a float to an int
+
+    //exp(x) => exponential E raises to X power, E = 2.17828
+    //log(x) => logarithim, inverse of the exp
+   double result;
+    result = pow(5, 3);  //5 * 5 * 5 = 125
+    result = pow(125, -3); //cube root of 125
+
+    result = sqrt(1000); //10
+
+    result = sqrt(pow(5, 2)); // 5
+
+    result = abs(-10); //10
+    result = abs(10); //10
+
+    result = ceil(14.5); // 15
+    result = floor(3.1); //3
+
+    result = round(14.5); //15
+    result = trunc(3.1); //3
+    result = (int)14.5; 
+
+    result = exp(4);
+    result = log(exp(4)); // 4
 
 }
 
