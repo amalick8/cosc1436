b396e364b2709bb7f62bea90ca359edde6772552
ammar_malick_8 <ammarmalick2006@gmail.com>
2025-09-23 17:44:05 -0500

    update 2

--- classwork/Chapter4/Chapter4/Chapter4.cpp
+++ classwork/Chapter4/Chapter4/Chapter4.cpp
@@ -47,7 +47,60 @@ int main()
     std::cout << "Enter the optional user rating (1.0-10.0): ";
     std::cin >> userRating;
 
-    
- 
+    std::cout << "Is this a classic (Y/N)? ";
+    std::string input;
+    std::cin >> input;
 
+    if (_strcmpi(input.c_str(), "Y") == 0)
+        isClassic = true;
+    if (_strcmpi(input.c_str(), "N") == 0)
+        isClassic = false;
+
+    //TODO: Handle other values
 };
+
+void relationalDemo()
+{
+    std::cout << "Enter two values: ";
+
+    int left, right;
+    std::cin >> left >> right;
+
+    bool areEqual = left == right;
+    std::cout << "areEqual = " << areEqual << std::endl;
+
+    // Relational operators ::= > >= < <= == !=
+    // Warning 1 - Equality and equals are similar and work in the same situations
+    // Warning 2 - Be very, very careful of floating point comparison for equality
+    //              Consider using >= or <= instead unless comparing to an integer
+    // Warning 3 - Strings compare by character values not by locale
+    bool areEqual = left = right;
+    std::cout << "areEqual = " << areEqual << std::endl;
+    areEqual = left == right;
+    std::cout << "areEqual = " << areEqual << std::endl;
+
+    std::cout << "> " << (left > right) << std::endl;
+    std::cout << "< " << (left < right) << std::endl;
+    std::cout << ">= " << (left >= right) << std::endl;
+    std::cout << "<= " << (left <= right) << std::endl;
+    std::cout << "== " << (left == right) << std::endl;
+    std::cout << "!= " << (left != right) << std::endl;
+
+    float someValue = (10 / 3.0) * 3.0;
+    float someOtherValue = (10 * 3.0) / 3.0;
+    areEqual = someValue == someOtherValue;
+    std::cout << areEqual << std::endl;
+
+    // Comparison works but is case sensitive
+    std::string name1 = "Bob";
+    std::string name2 = "bob";
+    std::cout << (name1 == name2) << std::endl;
+
+    // To compare strings case insensitive use the strcmpi
+    //  > 0 ::= left > right
+    //  < 0 ::= left < right
+    //  == 0 ::= left == right
+    areEqual = _strcmpi(name1.c_str(), name2.c_str()) == 0;
+
+
+}
